22028477c6999eb64a757890e86d411c3a0ab7a3
isaias3silva <isaias3silva@gmail.com>
2021-11-14 16:57:39 -0300

    Created file users.route.ts

---
+++ src/routes/users.route.ts
@@ -0,0 +1,15 @@
+import { NextFunction, Request, Response, Router } from "express";
+
+const usersRoute  = Router();
+
+usersRoute.get('/users', (req: Request, res: Response, next: NextFunction) => {
+    const users = [{ userName: 'Isaias'}];
+    res.status(200).send({users});
+});
+
+usersRoute.get('/users/:uuid', (req: Request<{ uuid: string }>, res: Response, next: NextFunction) => {
+    const uuid  = req.params.uuid;
+    res.status(200).send({ uuid });
+});
+
+export default usersRoute;
