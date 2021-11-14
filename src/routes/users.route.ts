1a4f9e3e486b77dc13600c3fa77731ae30994b59
isaias3silva <isaias3silva@gmail.com>
2021-11-14 17:09:34 -0300

    Alter file users.route.ts. add metode post

--- src/routes/users.route.ts
+++ src/routes/users.route.ts
@@ -1,15 +1,21 @@
 import { NextFunction, Request, Response, Router } from "express";
+import { StatusCodes } from "http-status-codes";
 
 const usersRoute  = Router();
 
 usersRoute.get('/users', (req: Request, res: Response, next: NextFunction) => {
     const users = [{ userName: 'Isaias'}];
-    res.status(200).send({users});
+    res.status(StatusCodes.OK).send({users});
 });
 
 usersRoute.get('/users/:uuid', (req: Request<{ uuid: string }>, res: Response, next: NextFunction) => {
     const uuid  = req.params.uuid;
-    res.status(200).send({ uuid });
+    res.status(StatusCodes.OK).send({ uuid });
+});
+
+usersRoute.post('/users', (req: Request, res: Response, next: NextFunction) => {
+    const newUser = req.body;
+    res.status(StatusCodes.CREATED).send(newUser);
 });
 
 export default usersRoute;
